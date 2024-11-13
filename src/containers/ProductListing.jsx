8d03c920392fa00b2424251fb8611a29bdeb27d2
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-13 21:44:13 +0530

    product listing make it good

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "./Header";
+import ProductComponent from "./ProductComponent";
 
 function ProductListing() {
-    const products=useSelector((state)=>state);
-    console.log(products)
+  // Assuming 'products' is part of your state, access it like this:
+  const products = useSelector((state) => state); // or adjust based on your actual state structure
+
+  console.log(products);
+
   return (
-    <div className='ui read container'>
-      <h1>Product Listing</h1>
+    <div className="sam">
+<ProductComponent/>
+
     </div>
-  )
+  );
 }
 
-export default ProductListing
+export default ProductListing;
