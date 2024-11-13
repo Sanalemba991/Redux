a5af2104409da466bbd24e0c04cb4c3ae8e2975d
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-13 21:43:58 +0530

    header making good

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import ProductListing from './ProductListing'
 
 function Header() {
   return (
+  <>
     <div className='ui fixed menu'>
       <div className='ui containner centre'>
 <h2>Sana Shop</h2>
+
       </div>
     </div>
+    <ProductListing/>
+    </>
   )
 }
 
