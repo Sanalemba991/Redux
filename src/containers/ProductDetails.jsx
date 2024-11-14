f90aa24e73a71ad1b3da260acd3aef4674c153c1
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-14 19:28:15 +0530

    error

--- src/containers/ProductDetails.jsx
+++ src/containers/ProductDetails.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import {selectedProduct} from '../redux/actions/productAction.js'
+ 
 function ProductDetails() {
+  const product =useSelector((state)=>state.product);
+
+  const {productId}=useParams();
+const dispatch=useDispatch();
+
+  const response = await axios
+  .get(`https://fakestoreapi.com/products/${id}`)
+  .catch((err) => {
+    console.log("Err: ", err);
+  });
+dispatch(selectedProduct(response.data));
+};
+
+  
   return (
     <div>
       <h1>Product Details</h1>
