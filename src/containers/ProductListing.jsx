8e373f5b0cb67f2fdf7165931adba49bcac907a1
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-14 18:57:55 +0530

    add Dispatch

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productAction";
 import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 function ProductListing() {
   // Assuming 'products' is part of your state, access it like this:
   const products = useSelector((state) => state); // or adjust based on your actual state structure
-
+const dispatch=useDispatch();
   console.log(products);
-  const fetchProducts=async()=>{
-    const response =
-  }
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+     dispatch(setProducts(response.data))
+  };
 
+  useEffect(() => {
+    fetchProducts();
+  }, []); 
   return (
     <div className="sam">
       <ProductComponent />
