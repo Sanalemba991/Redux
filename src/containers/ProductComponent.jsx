3b98d07552e90910727dfd60f045a8058963233a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-14 19:21:36 +0530

    css from chat gpt

--- src/containers/ProductComponent.jsx
+++ src/containers/ProductComponent.jsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import './ProductComponent.css';  // Importing custom CSS
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-      <div className="four wide column" key={id}>
-        <Link to={`/product/${id}`}>
-          <div className="ui link cards">
-            <div className="card">
-              <div className="image">
-                <img src={image} alt={title} />
-              </div>
-              <div className="content">
-                <div className="header">{title}</div>
-                <div className="meta price">$ {price}</div>
-                <div className="meta">{category}</div>
-              </div>
+      <div className="product-card" key={id}>
+        <Link to={`/product/${id}`} className="product-link">
+          <div className="ui card">
+            <div className="image">
+              <img src={image} alt={title} />
+            </div>
+            <div className="content">
+              <div className="header">{title}</div>
+              <div className="meta price">${price}</div>
+              <div className="meta">{category}</div>
             </div>
           </div>
         </Link>
       </div>
     );
   });
-  return <>{renderList}</>;
+
+  return <div className="product-list">{renderList}</div>;
 };
 
 export default ProductComponent;
