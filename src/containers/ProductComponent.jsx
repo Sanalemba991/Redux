76c9174623a0bf53747a2d5832bd5e42c62309f4
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-14 17:29:50 +0530

    making good

--- src/containers/ProductComponent.jsx
+++ src/containers/ProductComponent.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function ProductComponent() {
+  const products = useSelector((state) => state);
+  const {id,title}=product;
   return (
-    <div>
-      <h1>Product Component</h1>
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{title}</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductComponent
+export default ProductComponent;
